aa0acaceee7a155033dc2ba24b49d18617fa95b4
Valentin Aebi <valentin.aebi@epfl.ch>
2023-10-22 16:19:52 +0200

    Keep column indices in preprocessor mapping

--- javaPreprocessor/src/main/scala/Preprocessor.scala
+++ javaPreprocessor/src/main/scala/Preprocessor.scala
@@ -6,9 +6,9 @@ import scala.util.Using
 final class Preprocessor {
 
   type FileName = String
-
-  private type Code = String
-  private type LineIdx = Int
+  type Code = String
+  type LineIdx = Int
+  type ColIdx = Int
 
   def preprocess(originalFileName: FileName, newFileName: FileName, mappingFileName: FileName): Unit = {
     Using(Source.fromFile(originalFileName)){ bufSrc =>
@@ -17,46 +17,49 @@ final class Preprocessor {
     }
   }
 
-  def preprocess(codeLines: Seq[String]): (Code, Map[LineIdx, LineIdx]) = {
-    val code = codeLines.map(_.trim).mkString("\n")
+  def preprocess(codeLines: Seq[String]): (Code, Map[LineIdx, (LineIdx, ColIdx)]) = {
+    val code = codeLines.mkString("\n")
     makeOneStatementPerLine(code)
   }
 
   // assumption: code has been parsed in such a way that there is no Windows or old MacOS end-of-line anymore (i.e. no \r)
-  // assumption: lines have been trimmed
-  private def makeOneStatementPerLine(code: Code): (Code, Map[LineIdx, LineIdx]) = {
+  private def makeOneStatementPerLine(code: Code): (Code, Map[LineIdx, (LineIdx, ColIdx)]) = {
 
     val codeB = new StringBuilder()
-    val linesMappingB = Map.newBuilder[LineIdx, LineIdx]
+    val linesMappingB = Map.newBuilder[LineIdx, (LineIdx, ColIdx)]
     val codeIter = new OnTheFlyTokenizeIterator(code)
+
+    var origFileLineIdx = 1
+    var formattedFileLineIdx = 1
+    var origFileColIdx = 1
 
     // TODO recognize if, while, etc.
 
     def deleteSpaces(): Unit = {
       while (codeIter.peekNextChar.isSpaceChar && codeIter.hasNext) {
         codeIter.consume()
+        origFileColIdx += 1
       }
     }
 
-    var origFileLineIdx = 1
-    var formattedFileLineIdx = 1
-
-    linesMappingB.addOne(origFileLineIdx -> formattedFileLineIdx)
+    deleteSpaces()
+    linesMappingB.addOne(formattedFileLineIdx -> (origFileLineIdx, origFileColIdx))
     while (codeIter.hasNext){
       val curr = codeIter.consume()
+      origFileColIdx += curr.length
       codeB.append(curr)
       curr match {
         case ";" | "{" | "}" if codeIter.hasNext && codeIter.peekNextChar != '\n' =>
           deleteSpaces()
           codeB.append('\n')
           formattedFileLineIdx += 1
-          linesMappingB.addOne(formattedFileLineIdx -> origFileLineIdx)
+          linesMappingB.addOne(formattedFileLineIdx -> (origFileLineIdx, origFileColIdx))
         case "\n" =>
-          deleteSpaces()
-          require(!codeIter.peekNextChar.isSpaceChar, s"invalid code input: line $origFileLineIdx has not been trimmed")
           origFileLineIdx += 1
           formattedFileLineIdx += 1
-          linesMappingB.addOne(formattedFileLineIdx -> origFileLineIdx)
+          origFileColIdx = 1
+          deleteSpaces()
+          linesMappingB.addOne(formattedFileLineIdx -> (origFileLineIdx, origFileColIdx))
         case _ => ()
       }
     }
