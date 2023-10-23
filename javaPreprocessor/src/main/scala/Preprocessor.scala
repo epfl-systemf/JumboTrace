3e48ae15dbb8395a3566ac9214429998cfe4cf5a
Valentin Aebi <valentin.aebi@epfl.ch>
2023-10-23 15:48:22 +0200

    Early prototype of a more rigourous preprocessor

--- javaPreprocessor/src/main/scala/Preprocessor.scala
+++ javaPreprocessor/src/main/scala/Preprocessor.scala
@@ -1,70 +1,165 @@
 package javaPreprocessor
 
+import com.github.javaparser
+import com.github.javaparser.ParserConfiguration.LanguageLevel
+import com.github.javaparser.ast.`type`.PrimitiveType
+import com.github.javaparser.ast.body.VariableDeclarator
+import com.github.javaparser.ast.expr.*
+import com.github.javaparser.ast.stmt.*
+import com.github.javaparser.ast.visitor.GenericVisitor
+import com.github.javaparser.ast.{CompilationUnit, ImportDeclaration, Node, NodeList}
+import com.github.javaparser.printer.lexicalpreservation.LexicalPreservingPrinter
+import com.github.javaparser.{JavaParser, ParserConfiguration, Position, Problem}
+
+import java.util.concurrent.atomic.{AtomicInteger, AtomicLong}
 import scala.io.Source
 import scala.util.Using
 
-object Preprocessor {
+final class Preprocessor {
 
   type FileName = String
   type Code = String
   type LineIdx = Int
   type ColIdx = Int
 
-  def preprocess(originalFileName: FileName, newFileName: FileName, mappingFileName: FileName): Unit = {
-    Using(Source.fromFile(originalFileName)){ bufSrc =>
-      val formattedCode = preprocess(bufSrc.getLines().toSeq)
-      // TODO
-    }
+  private val languageLevel = LanguageLevel.JAVA_17
+
+  private val varNameIdx = new AtomicLong(0)
+  private def nextVarName(constructDescr: String): String = {
+    s"___$constructDescr" + varNameIdx.incrementAndGet()
   }
 
-  def preprocess(codeLines: Seq[String]): (Code, Map[LineIdx, (LineIdx, ColIdx)]) = {
-    val code = codeLines.mkString("\n")
-    makeOneStatementPerLine(code)
-  }
+  def preprocess(originalFileName: FileName, newFileName: FileName, mappingFileName: FileName): Seq[Problem] = {
+    Using(Source.fromFile(originalFileName)) { bufSrc =>
+      val config = new ParserConfiguration().setLanguageLevel(languageLevel)
+      val parser = new JavaParser(config)
+      val result = parser.parse(bufSrc.reader())
+      if (result.isSuccessful) {
+        val cu = result.getResult.get()
+        preprocess(cu)
 
-  // assumption: code has been parsed in such a way that there is no Windows or old MacOS end-of-line anymore (i.e. no \r)
-  private def makeOneStatementPerLine(code: Code): (Code, Map[LineIdx, (LineIdx, ColIdx)]) = {
+        ??? // TODO
 
-    val codeB = new StringBuilder()
-    val linesMappingB = Map.newBuilder[LineIdx, (LineIdx, ColIdx)]
-    val codeIter = new OnTheFlyTokenizeIterator(code)
-
-    var origFileLineIdx = 1
-    var formattedFileLineIdx = 1
-    var origFileColIdx = 1
-
-    // TODO recognize if, while, etc.
-    // Plan: transform e.g.  if (x == 0){ ... }  into  boolean ___if_cond_line_78___; if (___if_cond_line_78___ = x == 0){ ... }
-
-    def deleteSpaces(): Unit = {
-      while (codeIter.peekNextChar.isSpaceChar && codeIter.hasNext) {
-        codeIter.consume()
-        origFileColIdx += 1
+      } else {
+        val seqB = Seq.newBuilder[Problem]
+        result.getProblems.forEach(seqB.addOne)
+        seqB.result()
       }
-    }
-
-    deleteSpaces()
-    linesMappingB.addOne(formattedFileLineIdx -> (origFileLineIdx, origFileColIdx))
-    while (codeIter.hasNext){
-      val curr = codeIter.consume()
-      origFileColIdx += curr.length
-      codeB.append(curr)
-      curr match {
-        case ";" | "{" | "}" if codeIter.hasNext && codeIter.peekNextChar != '\n' =>
-          deleteSpaces()
-          codeB.append('\n')
-          formattedFileLineIdx += 1
-          linesMappingB.addOne(formattedFileLineIdx -> (origFileLineIdx, origFileColIdx))
-        case "\n" =>
-          origFileLineIdx += 1
-          formattedFileLineIdx += 1
-          origFileColIdx = 1
-          deleteSpaces()
-          linesMappingB.addOne(formattedFileLineIdx -> (origFileLineIdx, origFileColIdx))
-        case _ => ()
-      }
-    }
-    (codeB.toString(), linesMappingB.result())
+    }.get
   }
 
+  def preprocess(cu: CompilationUnit): Unit = {
+    handleWhileLoops(cu)
+    handleDoWhileLoops(cu)
+    handleIf(cu)
+    handleForLoops(cu)  // must be performed after handleWhileLoops
+  }
+
+  private def handleForLoops(cu: CompilationUnit): Unit = {
+    cu.findAll(classOf[ForStmt]).forEach { forStmt =>
+      val inStats = new NodeList[Statement]()
+      inStats.add(forStmt.getBody)
+      inStats.addAll(forStmt.getUpdate.stream().map(new ExpressionStmt(_)).toList)
+      val outStats = new NodeList[Statement]()
+      val condVarName = nextVarName("for_cond")
+      outStats.add(new ExpressionStmt(new VariableDeclarationExpr(PrimitiveType.booleanType(), condVarName)))
+      outStats.addAll(forStmt.getInitialization.stream().map(new ExpressionStmt(_)).toList)
+      outStats.add(new WhileStmt(
+        new AssignExpr(new NameExpr(condVarName), forStmt.getCompare.orElse(new BooleanLiteralExpr(true)), AssignExpr.Operator.ASSIGN),
+        new BlockStmt(inStats)
+      ))
+      forStmt.replace(new BlockStmt(outStats))
+    }
+  }
+
+  private def handleWhileLoops(cu: CompilationUnit): Unit = {
+    cu.findAll(classOf[WhileStmt]).forEach { whileStmt =>
+      val outStats = new NodeList[Statement]()
+      val condVarName = nextVarName("while_cond")
+      outStats.add(new ExpressionStmt(new VariableDeclarationExpr(PrimitiveType.booleanType(), condVarName)))
+      outStats.add(new WhileStmt(
+        new AssignExpr(new NameExpr(condVarName), whileStmt.getCondition, AssignExpr.Operator.ASSIGN),
+        whileStmt.getBody
+      ))
+      whileStmt.replace(new BlockStmt(outStats))
+    }
+  }
+
+  private def handleDoWhileLoops(cu: CompilationUnit): Unit = {
+    cu.findAll(classOf[DoStmt]).forEach { doStmt =>
+      val outStats = new NodeList[Statement]()
+      val condVarName = nextVarName("do_while_cond")
+      outStats.add(new ExpressionStmt(new VariableDeclarationExpr(PrimitiveType.booleanType(), condVarName)))
+      outStats.add(new DoStmt(
+        doStmt.getBody,
+        new AssignExpr(new NameExpr(condVarName), doStmt.getCondition, AssignExpr.Operator.ASSIGN)
+      ))
+      doStmt.replace(new BlockStmt(outStats))
+    }
+  }
+
+  private def handleIf(cu: CompilationUnit): Unit = {
+    cu.findAll(classOf[IfStmt]).forEach { ifStmt =>
+      val outStats = new NodeList[Statement]()
+      val condVarName = nextVarName("if_cond")
+      outStats.add(new ExpressionStmt(new VariableDeclarationExpr(PrimitiveType.booleanType(), condVarName)))
+      outStats.add(new IfStmt(
+        new AssignExpr(new NameExpr(condVarName), ifStmt.getCondition, AssignExpr.Operator.ASSIGN),
+        ifStmt.getThenStmt,
+        if ifStmt.getElseStmt.isPresent then ifStmt.getElseStmt.get() else null
+      ))
+      ifStmt.replace(new BlockStmt(outStats))
+    }
+  }
+
+  // TODO handle switch
+
+}
+
+@main def tempTestMain(): Unit = {
+  val input =
+    """
+      |class Foo {
+      |
+      |   void foo(int x, int y, int z){
+      |      for (int i = 0; i < 10; i++){
+      |         System.out.println(i);
+      |      }
+      |      if (x == y){
+      |         x = z;
+      |      }
+      |      while (z < y){
+      |         z += 2;
+      |      }
+      |      do {
+      |         if (x % 2 == 0){
+      |            y += 1;
+      |         } else {
+      |            z += 2;
+      |         }
+      |      } while (z+y*x % 3 != 0);
+      |      if (x == 1){
+      |         y = 2;
+      |      } else if (y == 15){
+      |         y = 11;
+      |      } else if (z == 42){
+      |         y = x;
+      |      } else {
+      |         y = z-x;
+      |      }
+      |   }
+      |}
+      |""".stripMargin
+  end input
+  val parseRes = (new JavaParser(new ParserConfiguration().setLanguageLevel(LanguageLevel.JAVA_17))).parse(input)
+  parseRes.getProblems.forEach(System.err.println(_))
+  assert(parseRes.isSuccessful)
+  val cu = parseRes.getResult.get()
+  println(cu)
+  println("\n ---------------------------- \n")
+  (new Preprocessor()).preprocess(cu)
+  println(cu)
+//  cu.findAll(classOf[Node]).forEach { node =>
+//    println(s"${node.getClass.getSimpleName} ${node.getRange}")
+//  }
 }
