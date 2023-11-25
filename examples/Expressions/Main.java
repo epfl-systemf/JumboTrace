e84d893aec83a4c98d855e69f1bcf01369864a9e
Valentin Aebi <valentin.aebi@epfl.ch>
2023-11-25 16:11:47 +0100

    Additional example: basic expressions

---
+++ examples/Expressions/Main.java
@@ -0,0 +1,16 @@
+
+public final class Main {
+
+    public static void main(String[] args) {
+        var a = Integer.parseInt(args[0]);
+        var b = Integer.parseInt(args[1]);
+        var c = Integer.parseInt(args[2]);
+        var x = f(a, b*c + 2*a) + 5 + b;
+        System.out.println(x + 2);
+    }
+    
+    static int f(int x, int y){
+        return x*x*y + x/y + 1;
+    }
+    
+}
