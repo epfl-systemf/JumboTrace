c47ce5d65b378e38c805da6f78d760b941d28fe5
Valentin Aebi <valentin.aebi@epfl.ch>
2023-11-30 10:07:03 +0100

    Prevent recursive logging + Automate compilation of ___JumboTrace___

---
+++ injected/script.py
@@ -0,0 +1,15 @@
+import shutil
+import os
+
+# Copy all the .class files in the current directory to the examples
+
+examples_dir_name = "examples"
+
+if __name__ == "__main__":
+
+    for example in os.listdir(f"../{examples_dir_name}"):
+        for classFile in os.listdir("."):
+            if classFile.endswith(".class"):
+                target_path = os.path.join("..", examples_dir_name, example, classFile)
+                open(target_path, "w+").close()  # create file if needed
+                shutil.copyfile(classFile, target_path)
