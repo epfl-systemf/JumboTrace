bc665fa9b5453da61ba86232d4e680333575cc70
Valentin Aebi <valentin.aebi@epfl.ch>
2023-12-09 23:30:13 +0100

    Add example involving assignments

---
+++ examples/IdentAmbiguous/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+
+    static int foo = 42;
+
+    int bar = 75;
+
+    void setBar(int x){
+        bar = x;
+    }
+
+    int getBar(){
+        return bar;
+    }
+
+
+    public static void main(String[] args) {
+        var m = new Main();
+        System.out.println(m.bar);
+        var x = foo;
+        System.out.println(x);
+        System.out.println(foo);
+        x = x + x*x;
+        System.out.println(x);
+        System.out.println(m.bar);
+        m.bar = x;
+        System.out.println(m.getBar());
+        m.setBar(101);
+        System.out.println(m.bar);
+        x += 15;
+        System.out.println(foo);
+        foo = x;
+        System.out.println(foo);
+        System.out.println(Inner.baz);
+        Inner.baz = 32;
+        x = 19;
+        x = 2*x;
+        System.out.println(Inner.baz);
+    }
+
+    static class Inner {
+        static int baz = -1;
+    }
+
+}
